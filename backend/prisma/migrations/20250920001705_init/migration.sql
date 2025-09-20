b414707ebec14a5c23d8702b52c05736e01efcee
khangdepzaivodich <phantuankhanghcmue@gmail.com>
2025-09-20 07:59:49 +0700

    Update views and schemas

---
+++ backend/prisma/migrations/20250920001705_init/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `v_chitietdonhang` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `v_chitietsanpham` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `v_donhang_tinhtrang` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `v_nhaphang` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `v_voucher_donhang` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."v_chitietdonhang";
+
+-- DropTable
+DROP TABLE "public"."v_chitietsanpham";
+
+-- DropTable
+DROP TABLE "public"."v_donhang_tinhtrang";
+
+-- DropTable
+DROP TABLE "public"."v_nhaphang";
+
+-- DropTable
+DROP TABLE "public"."v_voucher_donhang";
